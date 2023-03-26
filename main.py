d4364320492bc95e9c1c892fe4850f8d7e7e407e
Joel Ong <52844020+ongkahyuan@users.noreply.github.com>
2023-03-26 23:25:14 +0800

    Resolve warning, precompute

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import modules.vis as vis
 import pandas as pd
 import datetime #imported date time library
 
-df = pd.read_csv("./trimmed.csv")
+df = pd.read_csv("./trimmed.csv", low_memory=False)
 
 gd = getData.GetData(df, pd.to_datetime("2022-07-01"), pd.to_datetime("2022-08-01")) #changed string to date time format
 
