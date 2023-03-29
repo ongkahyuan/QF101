35b7da24c6676430c14a4372feed64b374e190d1
Vena Chandra Salim <venasalim@Venas-MacBook-Pro-2.local>
2023-03-29 13:02:51 +0800

    added v2 of model

--- modules/model.py
+++ modules/model.py
@@ -1,6 +1,51 @@
 # Binomial Tree Option Pricing (nested list with for loop)
 import math
+import numpy as np
 # T-t -> tau
+
+def binomial_tree_option_pricing(S, K, r, q, tau, sigma, N=100):
+    """
+   Currently overpricing the option prices slightly, might be because there is no discount factor
+    """
+
+    delta_t = tau / N
+    u = np.exp(sigma * np.sqrt(delta_t))
+    d = 1 / u
+    p = (np.exp((r - q) * delta_t) - d) / (u - d)
+
+    # Build the binomial tree
+    stock_price = np.zeros((N+1, N+1))
+    stock_price[0, 0] = S
+    for i in range(1, N+1):
+        stock_price[i, 0] = stock_price[i-1, 0] * u
+        for j in range(1, i+1):
+            stock_price[i, j] = stock_price[i-1, j-1] * d
+
+    # Calculate option prices at expiration
+    call_option = np.maximum(stock_price[N] - K, 0)
+    put_option = np.maximum(K - stock_price[N], 0)
+
+    # Work backwards through the tree to calculate option prices at each node
+    for i in range(N-1, -1, -1):
+        for j in range(i+1):
+            call_option[j] = (p * call_option[j+1] + (1-p) * call_option[j]) * np.exp(-r * delta_t)
+            put_option[j] = (p * put_option[j+1] + (1-p) * put_option[j]) * np.exp(-r * delta_t)
+            stock_price[i, j] = stock_price[i, j] 
+
+            # Check for early exercise (American options only)
+            exercise_value = K - stock_price[i, j] if put_option[j] > call_option[j] else stock_price[i, j] - K
+            if exercise_value > 0:
+                call_option[j] = exercise_value if call_option[j] < exercise_value else call_option[j]
+                put_option[j] = exercise_value if put_option[j] < exercise_value else put_option[j]
+
+    # Calculate final option prices
+    american_call_price = call_option[0]
+    european_call_price = call_option[0] * np.exp(-r * tau)
+    american_put_price = put_option[0]
+    european_put_price = put_option[0] * np.exp(-r * tau)
+
+    return (european_call_price, european_put_price, american_call_price,american_put_price)
+
 def model(S,K,r,q,tau,sigma,N=100):
    # 1. 
    deltaT=tau/N                           # delta T is the time step
