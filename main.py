ca943e03fdcf0779d640d445c1d72fe33aa60311
Kai Feng (Mini) <kaifeng_toh@mymail.sutd.edu.sg>
2023-03-30 20:31:10 +0800

    Updated main.py to run eval

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import modules.eval as eval
+import modules.eval as evaluate
 import modules.get_data as getData
 import modules.model as model
 import modules.vis as vis
 import pandas as pd
-import datetime #imported date time library
+from datetime import datetime #imported date time library
 
+
+startDate = datetime(2022,7,1)
+endDate = datetime(2022,8,1)
 df = pd.read_csv("./trimmed.csv", low_memory=False)
 
-gd = getData.GetData(df, pd.to_datetime("2022-07-01"), pd.to_datetime("2022-08-01")) #changed string to date time format
+gd = getData.GetData(df, startDate, endDate) #changed string to date time format
 
-print(gd.data)
+evaluator = evaluate.Eval(df,startDate,endDate)
 
+print(evaluator.compareModeltoMarket())
 # res = []
 
 # for contract in data:
